840d083c36350e7bde04a98604d70bb0730adfd7
Adam Fulop <adam_fulop@yahoo.com>
2025-04-22 18:48:04 +0200

    result page seems OK ...

--- lib/views/payment_result_page.dart
+++ lib/views/payment_result_page.dart
@@ -1,22 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:motorway_sticker_app/providers/providers.dart';
 
 class PaymentResultPage extends ConsumerWidget {
   const PaymentResultPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Summary'), centerTitle: true),
-        body: Column(
-          children: [
-            Placeholder(),
-            FilledButton(onPressed: () => context.go('/'), child: Text('OK')),
-          ],
+    final paymentResult = ref.watch(paymentResultProvider);
+
+    return PopScope(
+      // onWillPop: () async => false,
+      canPop: false,
+      child: SafeArea(
+        child: Scaffold(
+          body: paymentResult.when(
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error:
+                (error, _) =>
+                    _buildResultContent(context, error.toString(), ref),
+            data:
+                (result) => _buildResultContent(
+                  context,
+                  result == 'SUCCESS'
+                      ? 'SIKERES FIZETÉS'
+                      : 'SIKERTELEN FIZETÉS :-(',
+                  ref,
+                ),
+          ),
         ),
       ),
     );
+  }
+
+  Widget _buildResultContent(
+    BuildContext context,
+    String message,
+    WidgetRef ref,
+  ) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(message, style: Theme.of(context).textTheme.headlineMedium),
+          const SizedBox(height: 20),
+          FilledButton(
+            onPressed: () => _resetAndNavigateHome(ref, context),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _resetAndNavigateHome(WidgetRef ref, BuildContext context) {
+    ref.invalidate(selectedVignetteProvider);
+    ref.invalidate(selectedCountiesProvider);
+    ref.invalidate(orderDataProvider);
+    ref.invalidate(selectedCountiesOnMapProvider);
+    ref.read(paymentResultProvider.notifier).reset();
+    context.go('/');
   }
 }
